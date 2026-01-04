b8a1c261503de31ed98a5f7d0d5f15a4418b74ff
Dan Park <dan.park.primary@gmail.com>
2026-01-04 12:25:26 -0500

    password protect entire site

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route 
+          path="/" 
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          } 
+        />
         <Route 
           path="/holidays" 
           element={
